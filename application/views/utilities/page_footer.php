cb457ede8758f89a11c36007d2125a6d9ebf85bc
guillaume <g.marmo@hotmail.fr>
2018-01-08 14:46:01 +0100

    Page utilities

---
+++ application/views/utilities/page_footer.php
@@ -0,0 +1,4 @@
+        <footer>Copyright (c) 2018 Copyright Holder All Rights Reserved. Axelle Delomez, Guillaume Marmorat, Rayan Barama, Emile Canac. Projet de DUT | <!-- <a class="ml" href="">Mentions légales</a> --></footer>
+    </body>
+</html>
+
---
+++ application/views/utilities/page_head.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <!-- Base Tags -->
+    <meta charset="utf-8">
+    <title><?= $title ?></title>
+    <meta name="author" content="Axelle Delomez, Guillaume Marmorat, Rayan Barama, Emile Canac">
+    <meta name="description" content="Application de gestion de bibliotheque." />
+
+    <!-- Media -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="<?= base_url().'assets/css/master.css'?>">
+    <?php
+        // For specific environment
+        //if(isset($env) && $env === 'child'){
+        //
+        //}else if(isset($env) && $env === 'master'){
+        //
+        //}
+    ?>
+</head>
+<body>
