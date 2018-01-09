64abc2454a7fe32bd529d67c9e7e0e8561e836b5
Unknown <g.marmo@hotmail.fr>
2018-01-09 16:08:32 +0100

    Auto stash before merge of "guillaume" and "origin/guillaume"

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: guillaume
- * Date: 08/01/18
- * Time: 13:18
- */
 
 class Main extends CI_Controller
 {
---
+++ application/models/Eleve_model.php
@@ -0,0 +1,20 @@
+<?php
+
+class Eleve_model extends Utilisateur_model
+{
+
+    private $table;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+    public function setPastille(string $userID, string $pastille): bool
+    {
+        
+    }
+
+}
