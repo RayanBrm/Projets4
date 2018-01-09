8896da408f9795f0394cb5a80a74e28fd370a086
Barama Rayan <rayan.brm@gmail.com>
2018-01-09 18:49:06 +0100

    Idk
    I still don't know

--- application/views/utilities/page_footer.php
+++ application/views/utilities/page_footer.php
@@ -1,6 +1,7 @@
         <footer>Copyright (c) 2018 Copyright Holder All Rights Reserved. Axelle Delomez, Guillaume Marmorat, Rayan Barama, Emile Canac. Projet de DUT <!-- <a class="ml" href="">Mentions légales</a> --></footer>
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
         <script type="text/javascript" src="<?= base_url().'assets/js/materialize.min.js' ?>"></script>
+        <script type="text/javascript" src="assets/js/jquery.js"></script>
         <script>
             $(document).ready(function() {
                 $('select').material_select();
@@ -12,6 +13,8 @@
                 placeholder: 'Mot-clé',
                 secondaryPlaceholder: '+Un autre ?',
             });
+
+
         </script>
     </body>
 </html>
