f9287d36287862147e5aae1aa74cbe28f87d6d83
Unknown <g.marmo@hotmail.fr>
2018-01-10 12:33:12 +0100

    FIX ?

--- application/views/main/connexionEleve.php
+++ application/views/main/connexionEleve.php
@@ -6,12 +6,35 @@ $this->load->view('utilities/page_head',$data);
 $this->load->view('utilities/page_nav', $data);
 
 ?>
-</div>
-</div>
 
 
 <div class="container">
+
+
     <div class="row">
+        <div class="col s3">
+            <br>
+            <br>
+            <br>
+            <br>
+
+            <div class="input-field col s12">
+                <i class="material-icons prefix red-text">grade</i>
+                <select>
+                    <option value="" disabled selected>Classe</option>
+                    <option value="1">CM2A</option>
+                    <option value="2">CPE(lol)</option>
+                    <option value="3">CM11</option>
+                </select>
+            </div>
+
+        </div>
+
+        <div class="col s9">
+            <br>
+            <br>
+            <br>
+            <br>
         <?= $childs ?>
     </div>
 </div>
