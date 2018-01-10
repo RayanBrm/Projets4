9e7798f0e4745356d14ceb029af36d7d7c03f860
Barama Rayan <rayan.brm@gmail.com>
2018-01-10 11:47:16 +0100

    connexionEleve graphic fix , selecter fix

--- application/views/main/connexionEleve.php
+++ application/views/main/connexionEleve.php
@@ -6,6 +6,32 @@ $this->load->view('utilities/page_head');
 $this->load->view('utilities/page_nav');
 ?>
 
+<div class="row">
+
+    <div class="col s3">
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <div class="input-field col s12">
+            <i class="material-icons prefix red-text">grade</i>
+            <select>
+                <option value="" disabled selected>Classe</option>
+                <option value="1">CM2A</option>
+                <option value="2">CPE(lol)</option>
+                <option value="3">CM11</option>
+            </select>
+        </div>
+
+    </div>
+
+    <div class="col s9">
+        <br>
+        <br>
+        <br>
+        <br>
+
 <?php
     echo "<div class=\"row\">".
             "<div class='col i'>".
@@ -150,9 +176,11 @@ $this->load->view('utilities/page_nav');
             "</div>".
         "</div>";
 ?>
+</div>
+</div>
 
 <?php
+            $data['jquery']=includeJQUERY();
+            $this->load->view('utilities/page_footer',$data); ?>
 
-$this->load->view('utilities/page_footer');
-?>
 
