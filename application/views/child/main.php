0b7dbc03b0e8fbbb7e322a0e6a5c6c1dadca3760
guillaume <g.marmo@hotmail.fr>
2018-01-11 14:43:04 +0100

    Fix child ui, just not passing arguments

--- application/views/child/main.php
+++ application/views/child/main.php
@@ -1,14 +1,11 @@
 <?php
+    $data['title'] = $_SESSION['child']['prenom'].' '.$_SESSION['child']['nom'];
+    $data['env'] = 'childlog';
+    $this->load->view('utilities/page_head',$data);
+    $this->load->view('utilities/page_nav');
 
+    $data['books'] = $books;
+    $this->load->view('utilities/catalog_module',$data);
 
-$data['title'] = $_SESSION['child']['prenom'].' '.$_SESSION['child']['nom'];
-$data['env'] = 'childlog';
-$this->load->view('utilities/page_head',$data);
-$this->load->view('utilities/page_nav');
-
-$data['books'] = $books;
-$this->load->view('utilities/catalog_module',$data);
-
-
-$data['load'] = array('ajax','jquery.min','materialize.min','chips','catalogue');
-$this->load->view('utilities/page_footer');
+    $data['load'] = array('ajax','jquery.min','materialize.min','chips','catalogue');
+    $this->load->view('utilities/page_footer',$data);
