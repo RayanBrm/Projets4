e173ba45a46476bf218863a4e9033acfd187290f
Axelle <axelledelomez@gmail.com>
2018-01-11 13:51:03 +0100

    navbar trop mignonne mais attention c'est toujours turtle ?

--- application/views/child/main.php
+++ application/views/child/main.php
@@ -2,8 +2,11 @@
 
 
 $data['title'] = $_SESSION['child']['prenom'].' '.$_SESSION['child']['nom'];
+$data['env'] = 'childlog';
 $this->load->view('utilities/page_head',$data);
+$this->load->view('utilities/page_nav');
 
 dump($_SESSION);
+
 
 $this->load->view('utilities/page_footer');
